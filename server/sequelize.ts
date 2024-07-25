296cd21bd91d41b2a851019166d1064a2706bb87
Harshal Kahar <harshal.kahar@esparkbizmail.com>
2024-07-25 10:02:06 +0530

    server 🔥

--- server/sequelize.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const connection: Sequelize = new Sequelize("", "", "migration-ts-pg", {
-  host: "localhost",
-  dialect: "postgres",
-});
-
-export default connection;
